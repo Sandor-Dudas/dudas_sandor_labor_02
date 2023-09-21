f969e2f52d77b22060d781e9617789142134a383
infinity-nandi <nandorrexexel@gmail.com>
2023-09-21 12:21:17 +0200

    2 szinkron

--- 03.py
+++ 03.py
@@ -1,6 +1,7 @@
-#jövedelemszámítás
+# jövedelemszámítás
 print("jövedelemszámítás\n")
 kor = int(input("Hány éves vagy?"))
+gyerek = ""
 if kor > 25:
     gyerek = input("Van 3 gyereked és nő vagy (igen/nem)?")
 
@@ -12,5 +13,10 @@ if kor <= 25 or gyerek in ["igen", "Igen", "i", "I"]:
         szja = 0
 else:
     szja = brutto * 0.15
-
-print("SZJA:".ljust(30, "_"), str(int(szja)).rjust(10,"_"), sep="")
+print("Adók".center(40))
+print("SZJA:".ljust(30, "_"), str(int(szja)).rjust(10, "_"), sep="")
+print("Nyugdíj:".ljust(30, "_"), str(int(brutto * 0.1)).rjust(10, "_"), sep="")
+print("TB:".ljust(30, "_"), str(int(brutto * 0.07)).rjust(10, "_"), sep="")
+print("Mknélkül:".ljust(30, "_"), str(int(brutto * 0.015)).rjust(10, "_"), sep="")
+print("")
+print("Nettó:".ljust(30, "_"), str(int(brutto-brutto * 0.185-szja)).rjust(10, "_"), sep="")
