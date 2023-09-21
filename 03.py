e4b338b75b9e7eab485ffdfc8ee532bc91454037
infinity-nandi <nandorrexexel@gmail.com>
2023-09-21 11:54:10 +0200

    2 szinkron

---
+++ 03.py
@@ -0,0 +1,15 @@
+#jövedelemszámítás
+print("jövedelemszámítás\n")
+kor = int(input("Hány éves vagy?"))
+gyerek = input("Van 3 gyereked és nő vagy (igen/nem)?")
+
+brutto = int(input("Mennyi a bruttód"))
+if kor <= 25 or gyerek in ["igen", "Igen", "i", "I"]:
+    if brutto > 500000:
+        szja = (brutto-500000) * 0.15
+    else:
+        szja = 0
+else:
+    szja = brutto * 0.15
+
+print("SZJA:".ljust(30, "_"), str(int(szja)).rjust(10,"_"), sep="")
