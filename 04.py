8b5272c736de85d9b3b602c9a5a1b419b7693f27
infinity-nandi <nandorrexexel@gmail.com>
2023-09-28 12:15:35 +0200

    2 szinkron

--- 04.py
+++ 04.py
@@ -1,32 +1,40 @@
 #számologép
 print("Számológép")
 def adatkeres(tipus):
-    szam = input("Kérek egy számot: ")
-    while not szam.isnumeric():
-        print("Rossz érték!!!")
-        szam = input("Kérek egy számot: ")
-    szam = int(szam)
-    return szam
+    valasz = 0
+    if tipus == "sz":
+        valasz = input("Kérek egy számot: ")
+        while not valasz.isnumeric():
+            print("Rossz érték!!!")
+            valasz = input("Kérek egy számot: ")
+        valasz = int(valasz)
+    elif tipus == "m":
+        muvelet = input("Kérem a műveleti jelet (+,-,/,*)")
+        while muvelet not in ["+", "-", "*", "/"]:
+            print("Nem érvényes műveleti jel!!!")
+            muvelet = input("Kérem a műveleti jelet (+,-,/,*)")
+    return valasz
 
+def muvelet_vegrehalytasa():
+    eredmeny = 0
+    if muvelet == "+":
+        eredmeny = szam1 + szam2
+    elif muvelet == "-":
+        eredmeny = szam1 - szam2
+    elif muvelet == "*":
+        eredmeny = szam1 * szam2
+    elif muvelet == "/":
+        eredmeny = szam1 / szam2
+
+
+muvelet()
 
 # A program eleje
 print("Számológép")
 szam1 = adatkeres("sz")
-muvelet =input("Kérem a műveleti jelet (+,-,/,*)")
-while muvelet not in ["+", "-", "*", "/"]:
-    print("Nem érvényes műveleti jel!!!")
-    muvelet =input("Kérem a műveleti jelet (+,-,/,*)")
+muvelet = adatkeres("m")
 szam2 = adatkeres("sz")
 
-eredmeny = 0
-if muvelet == "+":
-    eredmeny = szam1 + szam2
-elif muvelet == "-":
-    eredmeny = szam1 - szam2
-elif muvelet == "*":
-    eredmeny = szam1 * szam2
-elif muvelet == "/":
-    eredmeny = szam1 / szam2
 
 print(str(szam1).rjust(50))
 print(muvelet, end="")
