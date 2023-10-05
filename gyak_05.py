390f5aeee941199a3f7001933ef1210d3d9327a6
infinity-nandi <nandorrexexel@gmail.com>
2023-10-05 11:49:14 +0200

    2 szinkron

--- gyak_05.py
+++ gyak_05.py
@@ -1,24 +1,34 @@
 # Beléptetőrendszer
 def regisztracio():
+    ok_regisztracio = True
     felhasznalonev()
     jelszo_kerese()
-    jelszo_ellenorzese()
-    ok_regisztracio = True
+    ciklus = 1
+    while not jelszo_ellenorzese():
+        ciklus += 1
+        if ciklus > 3:
+            ok_regisztracio = False
+            break
     return ok_regisztracio
 
 def felhasznalonev():
-    pass
+    felhasznalo_email =input("Kérem az email címét: ")
+    while " " in felhasznalo_email:
+        felhasznalo_email = input("Nem jó az email!!!\nKérem az email címét: ")
 
 def jelszo_kerese():
     pass
 
 def jelszo_ellenorzese():
-    pass
+    ok_jelszo = True
+    return ok_jelszo
 
 def beleptetes():
     pass
 
 
 #Innen indul a program
-regisztracio()
+felhasznalo_email = ""
+felhasznalo_jelszo = ""
+if regisztracio():
 beleptetes()
