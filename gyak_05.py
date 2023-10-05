124db91eaa32090138aa996e1051127fd1cee153
infinity-nandi <nandorrexexel@gmail.com>
2023-10-05 12:21:56 +0200

    2 szinkron

--- gyak_05.py
+++ gyak_05.py
@@ -13,12 +13,21 @@ def regisztracio():
 
 def felhasznalonev():
     felhasznalo_email =input("Kérem az email címét: ")
-    while " " in felhasznalo_email:
+    while " " in felhasznalo_email or "@" not in felhasznalo_email or "." not in felhasznalo_email :
         felhasznalo_email = input("Nem jó az email!!!\nKérem az email címét: ")
 
 def jelszo_kerese():
-    pass
+    felhasznalo_jelszo = input("Kérek egy jeszót (1,a,A, min 8 karakter):")
+    ok_jelszo = True
+    while ok_jelszo:
+        if len(felhasznalo_jelszo) < 8:
+            ok_jelszo = False
 
+        van = False
+        for i in range(len(felhasznalo_jelszo)):
+            if felhasznalo_jelszo[i].isnumeric():
+                van = True
+    felhasznalo_jelszo = input("Nem megfelelő a jelszó!!!\nKérek egy jeszót (1,a,A, min 8 karakter):")
 def jelszo_ellenorzese():
     ok_jelszo = True
     return ok_jelszo
@@ -30,5 +39,6 @@ def beleptetes():
 #Innen indul a program
 felhasznalo_email = ""
 felhasznalo_jelszo = ""
+jelszo_kerese()
 if regisztracio():
-beleptetes()
+    beleptetes()
