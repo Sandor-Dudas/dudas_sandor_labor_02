c6f7d7f13e072216f17bac50dbfd0e739659345e
infinity-nandi <nandorrexexel@gmail.com>
2023-10-05 11:24:41 +0200

    2 szinkron

---
+++ gyak_05.py
@@ -0,0 +1,14 @@
+# Beléptetőrendszer
+def regisztracio():
+    ok_regisztracio = True
+    return ok_regisztracio
+
+
+
+def rbeleptetes():
+    pass
+
+
+#Innen indul a program
+regisztracio()
+beleptetes()
