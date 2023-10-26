92d70addeebcc4a4157d164539b5df5ee0de522a
infinity-nandi <nandorrexexel@gmail.com>
2023-10-26 11:53:19 +0200

    2 szinkron

--- gyak_08.py
+++ gyak_08.py
@@ -1,2 +1,15 @@
 from tkinter import *
 
+belepes = Tk()
+belepes.title("Felhasználó beleptetes")
+
+felh_nev_cimke = Label(belepes, text="Felhasznalo neve (e-mail):")
+felh_jelszo_cimke = Label(belepes, text="Jelszó")
+
+felh_nev = Entry(belepes, width=30)
+
+felh_nev_cimke.grid(row=0, column=0)
+felh_jelszo_cimke.grid(row=1, column=0)
+felh_nev.grid(row=0, column=1)
+
+belepes.mainloop()
