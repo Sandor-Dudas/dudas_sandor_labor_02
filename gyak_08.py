e07f1f437533427cb1204dfe30e6e79360019a55
infinity-nandi <nandorrexexel@gmail.com>
2023-10-26 12:31:25 +0200

    2 szinkron

--- gyak_08.py
+++ gyak_08.py
@@ -1,28 +1,65 @@
 from tkinter import *
 
-def ok_gomb_kezelese():
-    belepes.destroy()
 
-def reg_gomb_kezelese():
-    belepes.destroy()
+def belepes_ablak():
+    def ok_gomb_kezelese():
+        belepes.destroy()
 
-belepes = Tk()
-belepes.title("Felhasználó beleptetes")
+    def reg_gomb_kezelese():
+        belepes.destroy()
 
-felh_nev_cimke = Label(belepes, text="Felhasznalo neve (e-mail):")
-felh_jelszo_cimke = Label(belepes, text="Jelszó:")
+    belepes = Tk()
+    belepes.title("Felhasználó beleptetes")
 
-felh_nev = Entry(belepes, width=30)
-felh_jelszo = Entry(belepes, width=20)
+    felh_nev_cimke = Label(belepes, text="Felhasznalo neve (e-mail):")
+    felh_jelszo_cimke = Label(belepes, text="Jelszó:")
 
-gomb_ok = Button(belepes, text="OK", command=ok_gomb_kezelese, width=10)
-gomb_reg = Button(belepes, text="Regisztrció", command=reg_gomb_kezelese)
+    felh_nev = Entry(belepes, width=30)
+    felh_jelszo = Entry(belepes, width=20)
 
-felh_nev_cimke.grid(row=0, column=0, padx=10, pady=20, sticky=E)
-felh_jelszo_cimke.grid(row=1, column=0, sticky=E, padx=10)
-felh_nev.grid(row=0, column=1, padx=10, sticky=W)
-felh_jelszo.grid(row=1, column=1, sticky=W, padx=10)
-gomb_ok.grid(row=2, column=0, pady=20)
-gomb_reg.grid(row=2, column=1)
+    gomb_ok = Button(belepes, text="OK", command=ok_gomb_kezelese, width=10)
+    gomb_reg = Button(belepes, text="Regisztrció", command=reg_gomb_kezelese)
 
-belepes.mainloop()
+    felh_nev_cimke.grid(row=0, column=0, padx=10, pady=20, sticky=E)
+    felh_jelszo_cimke.grid(row=1, column=0, sticky=E, padx=10)
+    felh_nev.grid(row=0, column=1, padx=10, sticky=W)
+    felh_jelszo.grid(row=1, column=1, sticky=W, padx=10)
+    gomb_ok.grid(row=2, column=0, pady=20)
+    gomb_reg.grid(row=2, column=1)
+
+    belepes.mainloop()
+
+def reg_ablak():
+    def ok_gomb_kezelese():
+        regisztracio.destroy()
+
+    def reg_gomb_kezelese():
+        regisztracio.destroy()
+
+    regisztracio = Tk()
+    regisztracio.title("Regisztráció")
+
+    felh_nev_cimke = Label(regisztracio, text="Felhaznalo neve:")
+    felh_jelszo_cimke = Label(regisztracio, text="Jelszo:")
+    felh_jelszo2_cimke = Label(regisztracio, text="Jelszo ismet:")
+
+    felh_nev = Entry(regisztracio, width=30)
+    felh_jelszo = Entry(regisztracio, width=20)
+    felh_jelszo2 = Entry(regisztracio, width=20)
+
+    gomb_ok = Button(regisztracio, text="OK", command=ok_gomb_kezelese)
+    gomb_jelszo = Button(regisztracio, text="jelszo", command=jelszo_gomb_kezelese)
+
+    felh_nev_cimke.grid(row=0, column=0)
+    felh_jelszo_cimke.grid(row=1, column=0)
+    felh_jelszo2_cimke.grid(row=2, column=0)
+    felh_nev.grid(row=0, column=1)
+    felh_jelszo.grid(row=1, column=1)
+    felh_jelszo2.grid(row=2, column=1)
+    gomb_ok.grid(row=3, column=0)
+    gomb_jelszo.grid(row=1, column=2)
+
+    regisztracio.mainloop()
+
+#belepes_ablak()
+reg_ablak()
