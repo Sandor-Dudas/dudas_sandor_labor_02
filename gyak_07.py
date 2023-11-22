dad3feb11dd5bab80e885c85bf59ed5391f8842a
Sandor-Dudas <144947107+Sandor-Dudas@users.noreply.github.com>
2023-11-22 10:56:21 +0100

    Delete gyak_07.py

--- gyak_07.py
+++
@@ -1,8 +0,0 @@
-import gyak_05
-'''gyak_05.felhasznalo_ellenorzese("valami@gmail.com")
-if gyak_05.beleptetes():
-    print("Ok, most már hozzáfársz a rendszerhez")
-else:
-    print("Nincs jogosultságod használni a rendszert!")'''
-
-print(gyak_05.jelszo_generalasa(10, True, True, True))
